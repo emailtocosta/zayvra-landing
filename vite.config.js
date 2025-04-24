a14b0f42253ff8343ddce78c5616ae491bed33a5
Douglas Benedict Costa <emailtocosta@gmail.com>
2025-04-25 01:25:11 +0600

    Removed index.js and cleaned structure

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Vite will look for index.html in the root
+  build: {
+    outDir: 'dist',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
