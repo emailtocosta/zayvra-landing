ff565df0cd37f5852699c6c1c6b6760e818df294
Douglas Benedict Costa <emailtocosta@gmail.com>
2025-04-25 03:12:45 +0600

    Fixed import path and pushing from correct directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ZayvraLandingPage from '../ZayvraLandingPage'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ZayvraLandingPage from './ZayvraLandingPage';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ZayvraLandingPage />
-  </React.StrictMode>,
-)
-
+  </React.StrictMode>
+);
